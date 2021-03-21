14b3544e731d0496e781b7a165425170bc4a97cf
mahmoud Diab <mhmoddiab2@gmail.com>
2021-03-21 16:00:30 +0200

    add handlers and models

---
+++ src/handlers/posts.js
@@ -0,0 +1,32 @@
+const postsModel = require("../model/posts");
+
+function getAllPosts(req, res, next) {
+  postsModel
+    .getAllPosts()
+    .then((allPosts) => {
+      res.send(allPosts.rows);
+    })
+    .catch(next);
+}
+
+function addNewPost(req, res, next) {
+  const newPost = req.body;
+  postsModel
+    .addNewPost(newPost)
+    .then(() => {
+      res.status(204).send("The Post ADDED :)");
+    })
+    .catch(next);
+}
+
+function deletePost(req, res, next) {
+  const postId = req.params.id;
+  postsModel
+    .deletePost(postId)
+    .then(() => {
+      res.status(204).send("The Post Has DELETED :(");
+    })
+    .catch(next);
+}
+
+module.exports = { getAllPosts, addNewPost, deletePost };
