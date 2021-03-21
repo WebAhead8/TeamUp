7d1be256cbf6df0480b09170311ad77653a35005
mahmoud Diab <mhmoddiab2@gmail.com>
2021-03-21 14:27:04 +0200

    add folders

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const PORT = process.env.PORT || 4000;
-const cors = require("cors");
-
-const server = express();
-server.use(cors());
-
-server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
