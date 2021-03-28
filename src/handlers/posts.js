2d4e72d5bce627258e8232b5975e2e3fa39986ec
BoshEee <bosheee.shwi@gmail.com>
2021-03-28 18:30:38 +0300

    posts by user

--- src/handlers/posts.js
+++ src/handlers/posts.js
@@ -1,8 +1,18 @@
-const postsModel = require('../model/posts');
+const postsModel = require("../model/posts");
 
 function getAllPosts(req, res, next) {
   postsModel
     .getAllPosts()
+    .then((allPosts) => {
+      res.send(allPosts.rows);
+    })
+    .catch(next);
+}
+
+function getUsersPost(req, res, next) {
+  const id = req.params.id;
+  postsModel
+    .getUsersPost(id)
     .then((allPosts) => {
       res.send(allPosts.rows);
     })
@@ -14,7 +24,7 @@ function addNewPost(req, res, next) {
   postsModel
     .addNewPost(newPost)
     .then(() => {
-      res.status(201).send('The Post ADDED :)');
+      res.status(201).send("The Post ADDED :)");
     })
     .catch(next);
 }
@@ -24,9 +34,9 @@ function deletePost(req, res, next) {
   postsModel
     .deletePost(postId)
     .then(() => {
-      res.status(204).send('The Post Has DELETED :(');
+      res.status(204).send("The Post Has DELETED :(");
     })
     .catch(next);
 }
 
-module.exports = { getAllPosts, addNewPost, deletePost };
+module.exports = { getAllPosts, addNewPost, deletePost, getUsersPost };
