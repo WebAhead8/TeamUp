6b76abb77d8a7300f50474a78be653cfe466a9f5
BoshEee <bosheee.shwi@gmail.com>
2021-03-31 22:09:39 +0300

    cleaning code

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -2,7 +2,6 @@ const { STATUS_CODES } = require("http");
 
 // eslint-disable-next-line no-unused-vars
 function handleError(error, req, res, next) {
-  console.error(error);
   const status = error.status || 500;
   res.status(status);
   const message = STATUS_CODES[status];
